208c39698c3bba6cf3d3bea6ab1f854892d626ee
Alexey Gromov <italianwind@gmail.com>
2014-01-03 17:01:59 +0400

    NEW Added custom AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,35 +1,22 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Config\Loader\LoaderInterface;
+use BF\FrontBundle\BFFrontBundle;
+use NS\CoreBundle\CoreKernel;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppKernel extends Kernel
+/**
+ * App Kernel
+ *
+ */
+class AppKernel extends CoreKernel
 {
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-        );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        return $bundles;
-    }
-
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-    }
+	/**
+	 * @return Bundle[]
+	 */
+	protected function getUserBundles()
+	{
+		return array(
+			new BFFrontBundle(),
+		);
+	}
 }
